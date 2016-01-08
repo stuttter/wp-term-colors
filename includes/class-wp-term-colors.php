9c0ac3ae9c986949c9d239b500ce3f0df18dc077
John James Jacoby <johnjamesjacoby@me.com>
2016-01-07 23:26:01 -0600

    Rely on base class for `db_version_key`.

--- includes/class-wp-term-colors.php
+++ includes/class-wp-term-colors.php
@@ -30,11 +30,6 @@ final class WP_Term_Colors extends WP_Term_Meta_UI {
 	public $db_version = 201601070001;
 
 	/**
-	 * @var string Database version
-	 */
-	public $db_version_key = 'wpdb_term_color_version';
-
-	/**
 	 * @var string Metadata key
 	 */
 	public $meta_key = 'color';
